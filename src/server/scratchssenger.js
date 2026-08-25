ce7f9880cd61e42e2f1f1b698c92156b814e208d
shaman2016scratch <g.ilkh@yandex.ru>
2026-08-25 13:18:44 +0300

    Update

--- src/server/scratchssenger.js
+++ src/server/scratchssenger.js
@@ -1,7 +1,7 @@
 import api from "../api.js";
 import DcgDB from "../util/db.js";
 import { DB_SECRET } from "../util/env.js";
-import { User, Result } from "scratchssenger/src/help/class.js"
+import { User, Result, Chat } from "scratchssenger/src/help/class.js"
 
 const DB = new DcgDB("dcg", "scratchssenger", DB_SECRET)
 
@@ -16,10 +16,10 @@ api.newGetter("get", "/scratchssenger/", (req, res) => {
     })
 })
 
-api.newGetter("get", "/scratchssenger/session/", async (req, res) => {
+api.newGetter("post", "/scratchssenger/session/", async (req, res) => {
     try {
         const { login, session } = req.body
-        const indexUsers = JSON.parse(DB.read("/users/index.json"))
+        const indexUsers = JSON.parse(await DB.read("/users/index.json"))
         if (!Object.keys(indexUsers).includes(login)) {
             return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
         }
@@ -38,7 +38,10 @@ api.newGetter("get", "/scratchssenger/session/", async (req, res) => {
             result: {
                 name: userJSON.name,
                 username: userJSON.username,
-                id: userJSON.id
+                id: userJSON.id,
+                joinedAt: user.joined,
+                lastActive: user.active,
+                role: user.role
             }
         })
     } catch (error) {
@@ -55,10 +58,10 @@ api.newGetter("get", "/scratchssenger/session/", async (req, res) => {
     }
 })
 
-api.newGetter("get", "/scratchssenger/session/chats/", async (req, res) => {
+api.newGetter("post", "/scratchssenger/session/chats/", async (req, res) => {
     try {
         const { login, session } = req.body
-        const indexUsers = JSON.parse(DB.read("/users/index.json"))
+        const indexUsers = JSON.parse(await DB.read("/users/index.json"))
         if (!Object.keys(indexUsers).includes(login)) {
             return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
         }
@@ -88,4 +91,204 @@ api.newGetter("get", "/scratchssenger/session/chats/", async (req, res) => {
             ip: IP
         })
     }
+})
+
+api.newGetter("get", "/scratchssenger/users/:target/", async (req, res) => {
+    const { target } = req.params
+    const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+    if (!Object.keys(indexUsers).includes(target)) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const user = new User(indexUsers[target])
+    return api.responseJson(res, 200, {
+        ok: true,
+        result: {
+            name: user.name,
+            username: user.username,
+            id: user.id,
+            joinedAt: user.joined,
+            lastActive: user.active
+        }
+    })
+})
+
+api.newGetter("post", "/scratchssenger/chats/:target/", async (req, res) => {
+    const { target } = req.params
+    const { login, session } = req.body
+    const indexChats = JSON.parse(await DB.read("/chats/index.json"))
+    if (!Object.keys(indexChats).includes(target)) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const chat = new Chat(indexChats[target])
+    if (chat.accessType === "private") {
+        const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+        if (!Object.keys(indexUsers).includes(login)) {
+            return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+        }
+        const user = new User(indexUsers[login])
+        if (user.session !== session) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is fake" }))
+        }
+        const isActual = await user.checkSession()
+        if (!isActual) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is old" }))
+        }
+        if (!chat.members.includes(login) ) {
+            return api.responseJson(res, 403, new Result(false, { error: "you is not in this chat" })) 
+        }
+        return api.responseJson(res, 200, {
+            ok: true,
+            result: {
+                name: chat.name,
+                username: chat.username,
+                id: chat.id,
+                admins: chat.admins,
+                members: chat.members,
+                type: chat.type
+            }
+        })
+    }
+    return api.responseJson(res, 200, {
+        ok: true,
+        result: {
+            name: chat.name,
+            username: chat.username,
+            id: chat.id,
+            admins: chat.admins,
+            members: chat.members,
+            type: chat.type
+        }
+    })
+})
+
+api.newGetter("post", "/scratchssenger/chats/:target/messages", async (req, res) => {
+    const { target } = req.params
+    const { login, session } = req.body
+    const indexChats = JSON.parse(await DB.read("/chats/index.json"))
+    if (!Object.keys(indexChats).includes(target)) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const chat = new Chat(indexChats[target])
+    if (chat.accessType === "private") {
+        const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+        if (!Object.keys(indexUsers).includes(login)) {
+            return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+        }
+        const user = new User(indexUsers[login])
+        if (user.session !== session) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is fake" }))
+        }
+        const isActual = await user.checkSession()
+        if (!isActual) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is old" }))
+        }
+        if (!chat.members.includes(login) ) {
+            return api.responseJson(res, 403, new Result(false, { error: "you is not in this chat" })) 
+        }
+        return api.responseJson(res, 200, {
+            ok: true,
+            result: chat.messages
+        })
+    }
+    return api.responseJson(res, 200, {
+        ok: true,
+        result: chat.messages
+    })
+})
+
+api.newGetter("post", "/scratchssenger/chats/:target/join", async (req, res) => {
+    const { target } = req.params
+    const { login, session } = req.body
+    const indexChats = JSON.parse(await DB.read("/chats/index.json"))
+    if (!Object.keys(indexChats).includes(target)) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const chat = new Chat(indexChats[target])
+    const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+    if (!Object.keys(indexUsers).includes(login)) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const user = new User(indexUsers[login])
+    if (user.session !== session) {
+        return api.responseJson(res, 403, new Result(false, { error: "token is fake" }))
+    }
+    const isActual = await user.checkSession()
+    if (!isActual) {
+        return api.responseJson(res, 403, new Result(false, { error: "token is old" }))
+    }
+    if (chat.members.includes(login) ) {
+        return api.responseJson(res, 404, new Result(false, { error: "you is in this chat" })) 
+    }
+    chat.members.push(user.id)
+    return api.responseJson(res, 200, {
+        ok: true
+    })
+})
+
+api.newGetter("get", "/scratchssenger/users/id/:target/", async (req, res) => {
+    const target = Number(req.params.target)
+    const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+    if (!Object.keys(indexUsers).length < target) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const user = new User(indexUsers[Object.keys(indexUsers)[target-1]])
+    return api.responseJson(res, 200, {
+        ok: true,
+        result: {
+            name: user.name,
+            username: user.username,
+            id: user.id,
+            joinedAt: user.joined,
+            lastActive: user.active
+        }
+    })
+})
+
+api.newGetter("post", "/scratchssenger/chats/id/:target/", async (req, res) => {
+    const target = Number(req.params.target)
+    const { login, session } = req.body
+    const indexChats = JSON.parse(await DB.read("/chats/index.json"))
+    if (Object.keys(indexChats).length+1 < target) {
+        return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+    }
+    const chat = new Chat(indexChats[Object.keys(indexChats)[target-1]])
+    if (chat.accessType === "private") {
+        const indexUsers = JSON.parse(await DB.read("/users/index.json"))
+        if (!Object.keys(indexUsers).includes(login)) {
+            return api.responseJson(res, 404, new Result(false, { error: "account not found" }))
+        }
+        const user = new User(indexUsers[login])
+        if (user.session !== session) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is fake" }))
+        }
+        const isActual = await user.checkSession()
+        if (!isActual) {
+            return api.responseJson(res, 403, new Result(false, { error: "token is old" }))
+        }
+        if (!chat.members.includes(login) ) {
+            return api.responseJson(res, 403, new Result(false, { error: "you is not in this chat" })) 
+        }
+        return api.responseJson(res, 200, {
+            ok: true,
+            result: {
+                name: chat.name,
+                username: chat.username,
+                id: chat.id,
+                admins: chat.admins,
+                members: chat.members,
+                type: chat.type
+            }
+        })
+    }
+    return api.responseJson(res, 200, {
+        ok: true,
+        result: {
+            name: chat.name,
+            username: chat.username,
+            id: chat.id,
+            admins: chat.admins,
+            members: chat.members,
+            type: chat.type
+        }
+    })
 })
